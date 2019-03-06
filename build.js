b34208185c339a44890d9e785141f71f1e4a0443
David J Holland <stooge213@hotmail.com>
2019-03-06 21:26:58 +1100

    :sparkles: add dark and bordered variants

--- build.js
+++ build.js
@@ -1,16 +1,31 @@
 const fs = require('fs')
-const workbenchColors = require('ayu/ayu-mirage-bordered.json').colors
 const tokenColors = require('Material-theme/themes/OneDark-Pro-bold.json').tokenColors
+const workbenchThemes = [
+  {
+    id: 'mirage',
+    name: 'Mirage',
+    colors: require('ayu/ayu-mirage.json').colors
+  },
+  {
+    id: 'mirage-bordered',
+    name: 'Mirage Bordered',
+    colors: require('ayu/ayu-mirage-bordered.json').colors
+  },
+  {
+    id: 'dark',
+    name: 'Dark',
+    colors: require('ayu/ayu-dark.json').colors
+  },
+  {
+    id: 'dark-bordered',
+    name: 'Dark Bordered',
+    colors: require('ayu/ayu-dark-bordered.json').colors
+  }
+]
 
-// le edits
-workbenchColors['editorGroup.emptyBackground'] = workbenchColors['editorGroup.background']
-workbenchColors['editorGroup.background'] = undefined
-
-workbenchColors['gitDecoration.conflictingResourceForeground'] = '#f27983b3'
-
+// begin syntax edits
 const jsVarColors = tokenColors.find(e => e.name === 'js variable readwrite')
 jsVarColors.scope = jsVarColors.scope.split(',').filter(s => s !== 'meta.object-literal.key').join()
-
 tokenColors.push({
     name: 'js object key',
     scope: 'meta.object-literal.key',
@@ -18,18 +33,22 @@ tokenColors.push({
       foreground: '#56b6c2'
     }
 })
-
 tokenColors.find(e => e.name === 'Attribute IDs').settings.fontStyle = ''
 tokenColors.find(e => e.name === 'Attribute class').settings.fontStyle = ''
-// end le edits
+// end syntax edits
 
-const theme = {
-    name: 'Ayu One Dark Pro',
+for (const { id, name, colors } of workbenchThemes) {
+  // begin workbench edits
+  colors['editorGroup.emptyBackground'] = colors['editorGroup.background']
+  colors['editorGroup.background'] = undefined
+  colors['gitDecoration.conflictingResourceForeground'] = colors['gitDecoration.deletedResourceForeground']
+  // end workbench edits
+  const theme = {
+    name: `Ayu One Dark Pro | ${name}`,
     type: 'dark',
-    colors: workbenchColors,
-    tokenColors: tokenColors
+    colors,
+    tokenColors
+  }
+  const themeJSON = JSON.stringify(theme, '', 2)
+  fs.writeFileSync(`./themes/ayu-one-dark-pro-${id}-color-theme.json`, themeJSON)
 }
-
-const themeJSON = JSON.stringify(theme, '', 2)
-
-fs.writeFileSync('./themes/ayu-one-dark-pro-color-theme.json', themeJSON)
